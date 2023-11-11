b1cdd8a78eb4497ef3c0f3855f7ea086d722a2a5
prafullKrRj <prafullkumar384@gmail.com>
2023-11-11 21:48:01 +0530

    Retry Action

--- app/src/main/java/com/example/bookhive/ui/screens/commons/Error.kt
+++ app/src/main/java/com/example/bookhive/ui/screens/commons/Error.kt
@@ -5,7 +5,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -24,20 +30,22 @@ fun Error(onRetry: () -> Unit) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
+        Icon(
             painter = painterResource(id = R.drawable.outline_error_outline_24),
             contentDescription = null,
-            modifier = Modifier.size(100.dp)
+            modifier = Modifier.size(100.dp),
+            tint = colorScheme.onSurfaceVariant
         )
         Text(
-            text = "Something went wrong",
+            text = "Something went wrong,\nCheck your internet connection",
             fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
         )
-        TextButton(onClick = {
+        Button(onClick = {
             onRetry()
-        }) {
-            Text(text = "Retry")
+        }, colors = ButtonDefaults.buttonColors(containerColor = colorScheme.secondaryContainer)) {
+            Text(text = "Retry", color = colorScheme.onSecondaryContainer)
         }
     }
 }
