9f94099d11770ca51bb6fdb29a3840a67738c8d3
prafullKrRj <prafullkumar384@gmail.com>
2023-11-12 10:45:42 +0530

    API

--- app/src/main/java/com/example/bookhive/network/BooksApiService.kt
+++ app/src/main/java/com/example/bookhive/network/BooksApiService.kt
@@ -1,5 +1,6 @@
 package com.example.bookhive.network
 
+import com.example.bookhive.constants.Constant.API_KEY
 import com.example.bookhive.model.BookResponse.BooksResponse
 import com.example.bookhive.model.BookResponse.Item
 import retrofit2.http.GET
@@ -11,17 +12,19 @@ interface BooksApiService {
     @GET("volumes")
     suspend fun getBooks(
         @Query("q") query: String,
-        @Query("startIndex") startIndex: Int
-      //  @Query("key") key: String = API_KEY
+        @Query("startIndex") startIndex: Int,
+        @Query("key") key: String = API_KEY
     ) : BooksResponse
 
     @GET("volumes/{bookId}")
     suspend fun getBookById(
-        @Path("bookId") bookId: String
+        @Path("bookId") bookId: String,
+        @Query("key") key: String = API_KEY
     ) : Item
 
     @GET("volumes")
     suspend fun getBookByName (
-        @Query("q") query: String
+        @Query("q") query: String,
+        @Query("key") key: String = API_KEY
     ) : BooksResponse
 }
